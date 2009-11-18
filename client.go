771fa71b1b3bada87686461e75e1df05236f90dd
Nacho Martín <nitram.ohcan@gmail.com>
2009-11-18 01:51:04 +0100

    step forward

--- client.go
+++ client.go
@@ -1,30 +1,53 @@
 package main
 
 import (
-        "net";
-        "log";
-        "os";
-        "flag";
-//        "fmt";
-//        "strings";
-        )
+	"net";
+	"log";
+	"flag";
+	"encoding/binary";
+	//        "fmt";
+	//        "strings";
+)
 
 var server = flag.String("s", "127.0.0.1:4009", "server address")
-var local  = flag.String("c", "127.0.0.1:4005", "client address")
+var local = flag.String("c", "127.0.0.1:4005", "client address")
 
-func main(){
-    flag.Parse();
-    addr, err := net.ResolveTCPAddr(*local);
-    if err != nil {
-        log.Exit("error resolving client:", err);
-    }
+func streq(s1 string, s2 string) bool {
+	if len(s1) != len(s2) {
+		log.Stdout("nomi");
+		log.Stdout(len(s1));
+		log.Stdout(len(s2));
+		return false;
+	}
+	for i := 0; i < len(s1); i++ {
+		if s1[i] != s2[i] {
+			return false
+		}
+	}
+	return true;
+}
 
-    conn, err := net.Dial("tcp","",*server);
-    if err != nil {
-        log.Exit("error resolving server", err);
-    }
-    b := make([]byte, 1024);
-    conn.Read(b);
-    os.Stdout.Write(b);
-    conn.Write(addr.IP);
+func main() {
+	flag.Parse();
+	addr, err := net.ResolveTCPAddr(*local);
+	if err != nil {
+		log.Exit("error resolving client:", err)
+	}
+	log.Stdout(addr.Port);
+	conn, err := net.Dial("tcp", "", *server);
+	if err != nil {
+		log.Exit("error resolving server", err)
+	}
+	b := make([]byte, 1024);
+	size, err := conn.Read(b);
+	if err != nil {
+		log.Exit("error reading ", err)
+	}
+	log.Stdout("server says: " + string(b[0:size]));
+	if !streq("hi", string(b[0:size])) {
+		log.Exit("Error initializing")
+	}
+	conn.Write(addr.IP);
+	binary.LittleEndian.PutUint32(b, uint32(addr.Port));
+	conn.Write(b[0:3]);
 }
--- server.go
+++ server.go
@@ -1,40 +1,44 @@
 package main
 
 import (
-        "net";
-        "log";
-        "strings";
-        "os";
-    )
+	"net";
+	"log";
+	"strings";
+	"fmt";
+	"encoding/binary";
+)
 
-func ProcessConn(conn *net.TCPConn){
-    log.Stdout("connected\n");
-    conn.Write(strings.Bytes("hi\n"));
-    b := make([]byte, 1024);
-    conn.Read(b[0:15]);
-    os.Stdout.WriteString(net.IP(b[0:15]).String());
-    conn.Close();
+func ProcessConn(conn *net.TCPConn) {
+	defer conn.Close();
+	log.Stdout("connected\n");
+	conn.Write(strings.Bytes("hi"));
+	ip16 := make([]byte, 16);
+	conn.Read(ip16);
+	ip := net.IP(ip16);
+	var port uint32;
+	portb := make([]byte, 4);
+	conn.Read(portb);
+	port = binary.LittleEndian.Uint32(portb);
+	log.Stdout("new client on: " + ip.String() + ":" + fmt.Sprintf("%d", port) + "\n");
 }
 
-func main(){
-    addr, err := net.ResolveTCPAddr("127.0.0.1:4009");
-    if err != nil {
-        log.Exit("error:", err);
-    }
-    listener, err := net.ListenTCP("tcp", addr);
-    if err != nil {
-        log.Exit("error", err);
-    }
-    //c := make(chan int);
-    for{
-        log.Stdout("Waiting for connections\n");
-        conn, err := listener.AcceptTCP();
-        if err != nil {
-            log.Stdout("error in Accept():", err);
-        }else{
-            go func () {
-                ProcessConn(conn);
-            }();
-        }
-    }
+func main() {
+	addr, err := net.ResolveTCPAddr("127.0.0.1:4009");
+	if err != nil {
+		log.Exit("error:", err)
+	}
+	listener, err := net.ListenTCP("tcp", addr);
+	if err != nil {
+		log.Exit("error", err)
+	}
+	//c := make(chan int);
+	for {
+		log.Stdout("Waiting for connections\n");
+		conn, err := listener.AcceptTCP();
+		if err != nil {
+			log.Stdout("error in Accept():", err)
+		} else {
+			go func() { ProcessConn(conn) }()
+		}
+	}
 }
